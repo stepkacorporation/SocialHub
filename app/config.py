8edb7a8331defeeb5765335c04345e2c0d95e0ed
Stepan <kornevstepan169@gmail.com>
2024-08-26 22:59:43 +0700

    Add JWT settings to config

--- app/config.py
+++ app/config.py
@@ -8,11 +8,19 @@ DOTENV = os.path.join(os.path.dirname(__file__), f'.env.{ENVIRONMENT}')
 
 
 class Settings(BaseSettings):
+    # PostgreSQL settings
     postgres_user: str = Field(..., alias='POSTGRES_USER')
     postgres_password: str = Field(..., alias='POSTGRES_PASSWORD')
     postgres_db: str = Field(..., alias='POSTGRES_DB')
     postgres_host: str = Field(..., alias='POSTGRES_HOST')
     postgres_port: str = Field(..., alias='POSTGRES_PORT')
+
+    # JWT settings
+    secret_key_access: str = Field(..., alias='SECRET_KEY_ACCESS')
+    secret_key_refresh: str = Field(..., alias='SECRET_KEY_REFRESH')
+    algorithm: str = Field(..., alias='ALGORITHM')
+    access_token_expire_minutes: int = Field(..., alias='ACCESS_TOKEN_EXPIRE_MINUTES')
+    refresh_token_expire_days: int = Field(..., alias='REFRESH_TOKEN_EXPIRE_DAYS')
 
     model_config = SettingsConfigDict(
         env_file=DOTENV,
---
+++ app/routers/auth/consts.py
@@ -0,0 +1,7 @@
+from app.config import settings
+
+SECRET_KEY_ACCESS = settings.secret_key_access
+SECRET_KEY_REFRESH = settings.secret_key_refresh
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+REFRESH_TOKEN_EXPIRE_DAYS = settings.refresh_token_expire_days
